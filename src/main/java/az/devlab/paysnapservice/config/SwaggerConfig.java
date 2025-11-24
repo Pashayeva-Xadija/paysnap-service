525556130c2dbb98ef949f68b9a01be0467142f8
Pashayeva-Xadija <xadijapashayeva@gmail.com>
2025-11-24 20:59:13 +0400

    Added Swagger security config and fixed OpenAPI setup

--- src/main/java/az/devlab/paysnapservice/config/SwaggerConfig.java
+++ src/main/java/az/devlab/paysnapservice/config/SwaggerConfig.java
@@ -1,4 +1,33 @@
 package az.devlab.paysnapservice.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("BearerAuth")
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .type(SecurityScheme.Type.HTTP);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("BearerAuth");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PaySnap — QR Payment API")
+                        .version("1.0")
+                        .description("Stripe-powered QR payment generator API."))
+                .addSecurityItem(securityRequirement)
+                .schemaRequirement("BearerAuth", securityScheme);
+    }
 }
